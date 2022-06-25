003f0b7a45d1219f75bd3e8f1c675346b3826733
Pilevich Sasha <pilevichsasha@gmail.com>
2022-06-25 19:20:03 +0300

    src

--- src/script.js
+++ src/script.js
@@ -11,149 +11,128 @@ let img;
 let avatar;
 let about;
 let info;
+const nextBtn = document.querySelector(".nextBtn");
+const URL_API = 'https://62b0c0c4e460b79df04c901b.mockapi.io/api';
 
+const tabs = document.querySelector('.tabs');
+//Элементы из модального окна "добавить на доску"
+const modalElement = document.querySelector('.modal');
+const imageWrapper = document.querySelector('.imageWrapper');
+
+//Кнопки из модального окна "добавить на доску"
+const btnOne = document.querySelector('.tab-btn-one');
+const btnTwo = document.querySelector('.tab-btn-two');
+const btnThree = document.querySelector('.tab-btn-three');
+
+//Контейнеры которые появляются при переключении досок
+const deskOne = document.getElementById(1);
+const deskTwo = document.getElementById(2);
+const deskThree = document.getElementById(3);
+
+//Массив и local storage для доски 1
+let arrDeskOne = [];
+if (localStorage.getItem('deskOne')) {
+    arrDeskOne = JSON.parse(localStorage.getItem('deskOne'));
+    arrDeskOne.forEach((item) => {
+        createDeskOne(item)
+    });
+}
+//Массив и local storage для доски 2
+let arrDeskTwo = [];
+if (localStorage.getItem('deskTwo')) {
+    arrDeskTwo = JSON.parse(localStorage.getItem('deskTwo'));
+    arrDeskTwo.forEach((item) => {
+        createDeskTwo(item)
+    });
+} 
+//Массив и local storage для доски 3
+let arrDeskThree = [];
+if (localStorage.getItem('deskThree')) {
+    arrDeskThree = JSON.parse(localStorage.getItem('deskThree'));
+    arrDeskThree.forEach((item) => {
+        createDeskThree(item)
+    });
+}
+
+//Функция для отображения элементов добавленных на доску 1
+function createDeskOne(obj) {
+    let imageOnDesk = document.createElement('img');
+    imageOnDesk.src = obj.src;
+    imageOnDesk.style.cssText = `
+    object-fit: cover;
+    width:100%;
+    height: auto;
+    border-radius:20px;
+    `;
+    deskOne.append(imageOnDesk)
+}
+
+//Функция для отображения элементов добавленных на доску 2
+function createDeskTwo(obj) {
+    let imageOnDesk = document.createElement('img');
+    imageOnDesk.src = obj.src;
+    imageOnDesk.style.cssText = `
+    object-fit: cover;
+    width:100%;
+    height: auto;
+    border-radius:20px;
+    `;
+    deskTwo.append(imageOnDesk)
+}
+
+//Функция для отображения элементов добавленных на доску 3
+function createDeskThree(obj) {
+    let imageOnDesk = document.createElement('img');
+    imageOnDesk.src = obj.src;
+    imageOnDesk.style.cssText = `
+    object-fit: cover;
+    width:100%;
+    height: auto;
+    border-radius:20px;
+    `;
+    deskThree.append(imageOnDesk)
+}
+
+//Массив и сохранение в local storage главной страницы
 let newImages = [];
-
 if (localStorage.getItem('gallery')) {
     newImages = JSON.parse(localStorage.getItem('gallery'));
-
     newImages.forEach((item) => {
         createPinterest(item)
     });
 } else {
-    for (let key in images) {
-        let gallery = {};
-        gallery.name = key;
-        gallery.description = images[key]['description'];
-        gallery.avatar = images[key]['avatar'];
-        gallery.visible = true;
-        newImages.push(gallery);
-        createPinterest(gallery);
-        localStorage.setItem('gallery', JSON.stringify(newImages));
-    }
+    createElement();
 }
 
-//Модальное окно "Добавить на доску"
-
-function openModal(event) {
-    const buttonModalAdd = document.querySelector('.modal__add');
-    let modalElement = document.querySelector('.modal__1');
-
-    console.log(modalElement);
-
-    modalElement.classList.add('show');
-    modalElement.classList.remove('hide');
-
-    //Модальное окно "Закрыть модальное окно"
-
-    const buttonModalClose= document.querySelector('.modal__close');
-    buttonModalClose.addEventListener('click', (event) => {
-        let item = event.target.parentElement;
-        modalElement.classList.remove('show');
-        modalElement.classList.add('hide');
-    });
-
-    
+//Функция получения всех данных с Mockapi
+async function getPinterest() {
+    const response = await fetch(`${URL_API}/pinterest`);
+    const pinterest = await response.json();
+    return pinterest;
+}
+function createElement() {
+    getPinterest().then((pinterest) => {
+        pinterest.forEach(({id, name, avatar, description, hashtag}) => {
+            let gallery = {};
+            gallery.id = id;
+            gallery.name = name;
+            gallery.description = description;
+            gallery.avatar = avatar;
+            gallery.hashtag = hashtag;
+            newImages.push(gallery);
+            createPinterest(gallery); 
+            localStorage.setItem('gallery', JSON.stringify(newImages));
+            });
+    })
 }
 
-
-
+//Функция создания всех элементов на главной странице
 function createPinterest(obj) {
-
     wrapper = document.createElement('div');
     wrapper.classList.add('wrapper');
-    wrapper.visible = obj.visible;
+    wrapper.setAttribute('data-key', obj.name);
+    wrapper.id = obj.id;
 
-    hoverMenu = document.createElement('div');
-    hoverMenu.classList.add('hoverMenu');
-
-    const btnStyle = `
-    width:70%;
-    height:20%;
-    border-radius: 40px;
-    color: white;
-    font-family: Open sans, 'sans-serif';
-    font-size: 14px;
-    background-color:brown;
-    cursor:pointer;
-    border: none;
-    `;
-
-    btnAdd = document.createElement('button');
-    btnAdd.classList.add('btnAdd');
-    btnAdd.textContent = 'Добавить на доску';
-    btnAdd.style.cssText = btnStyle;
-
-<<<<<<< search
-=======
-    btnAdd.addEventListener('click', (event) => openModal(event));
-    
->>>>>>> develop
-    btnPin = document.createElement('button');
-    btnPin.classList.add('btnPin');
-    btnPin.textContent = 'Скрыть пин со страницы';
-    btnPin.style.cssText = btnStyle;
-
-    btnPin.addEventListener('click', () => {
-        for (let i = 0; i < newImages.length; i++) {
-            newImages = newImages.filter((item) => {
-                if (item.name !== obj.name) {
-                    imageOut.addEventListener('click', (event) => {
-                        if (event.target.className != 'btnPin') return;
-                        let pane = event.target.closest('.wrapper');
-                        pane.style.opacity = '0.9';
-                        let hoverPin = document.createElement('div');
-                        hoverPin.classList.add('hoverPin');
-                        hoverPin.innerHTML = 'Вы больше не увидите этот пин'
-                        pane.append(hoverPin)
-                    });
-                    return item;
-                }
-            })
-        }
-        localStorage.setItem('gallery', JSON.stringify(newImages));
-    });
-
-
-    btnComplaine = document.createElement('button');
-    btnComplaine.classList.add('btnComplaine');
-    btnComplaine.textContent = 'Пожаловаться';
-    btnComplaine.style.cssText = btnStyle;
-
-    const popoutComplain = document.querySelector(".popout_complain");
-    const popoutContent = document.querySelector(".complain_content");
-    const cancelBtn = document.querySelector(".cancelBtn");
-    const nextBtn = document.querySelector(".nextBtn");
-    const nextModal = document.querySelector(".next_modal");
-    const nextModalClose = document.querySelector(".next_modal__close");
-
-
-    btnComplaine.addEventListener('click', function modalComplain() {
-        popoutComplain.style.display = "block";
-    })
-
-    cancelBtn.addEventListener('click', function closeComplain() {
-        popoutComplain.style.display = "none";
-    })
-
-    
-    //    второе модальное
-
-<<<<<<< search
-
-=======
-    nextBtn.addEventListener('click', function openSecondModal(){
-        nextModal.style.display = "block";
-        popoutComplain.style.display = "none";
-
-    })
-
-    nextModalClose.addEventListener('click', function closeSecondModal(){
-        nextModal.style.display = "none";
-
-    })
-    
->>>>>>> develop
     imgContainer = document.createElement('div');
     imgContainer.classList.add('imgContainer');
 
@@ -171,63 +150,167 @@ function createPinterest(obj) {
     img = document.createElement('img');
     img.classList.add('image');
     img.setAttribute('data-key', obj.name);
-    img.src = 'images/' + obj.name + '.jpg';
+    img.src = obj.name;
+    img.id = obj.id;
     img.style.cssText = `
     object-fit: cover;
     width:100%;
     height: auto;
     border-radius:20px;
     `;
-
+    
     avatar = document.createElement('img');
-    avatar.setAttribute('data-key', obj.avatar);
+    avatar.setAttribute('data-key',obj.avatar );
     avatar.classList.add('bkgImage');
-    avatar.src = 'avatar/' + obj.avatar + '.jpg';
+    avatar.src = obj.avatar;
     avatar.style.cssText = `
     margin-right: 10px;
     width: 20%;
     height:20%;
     border-radius: 50%;
     `;
+
     info = document.createElement('h5');
     info.textContent = obj.description;
 
+    hoverMenu = document.createElement('div');
+    hoverMenu.classList.add('hoverMenu');
+
+    const btnStyle = `
+    width:50%;
+    height:20%;
+    border-radius: 20px;
+    color: white;
+    font-family: Open sans, 'sans-serif';
+    font-size: 10px;
+    background-color:brown;
+    cursor:pointer;
+    border: none;
+    text-align:center;
+    padding: 5px;
+    `;
+
+    btnAdd = document.createElement('button');
+    btnAdd.classList.add('btnAdd');
+    btnAdd.id = obj.id;
+    btnAdd.name = obj.name;
+    btnAdd.textContent = 'Добавить на доску';
+    btnAdd.style.cssText = btnStyle;
+
+    btnAdd.addEventListener('click', (event) => {
+        imageWrapper.innerHTML = '';//для того чтобы при повторном открытии модального окна не тянулась картинка с прошлого открытия
+        
+        if (event.target.closest('.wrapper') || event.target.getAttribute('data-key')){
+                currentImg = document.createElement('img');
+                currentImg.src = event.target.name;
+                currentImg.style.cssText = `
+                object-fit: cover;
+                width:100%;
+                height: auto;
+                border-radius:20px;`;
+                imageWrapper.append(currentImg);  
+        }
+        
+                modal();
+                openModal();    
+        })
+    //Функция модального окна "добавить на доску"
+        function modal() {
+        modalElement.addEventListener('click', (e) => {
+            if (
+            e.target === modalElement ||
+            e.target.getAttribute('data-close') == ''
+            ) {
+            closeModal();
+            }
+        });
+        }
+        
+        function openModal() {
+        modalElement.classList.add('show');
+        modalElement.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        }
+
+    //Подключение "скрыть пин"
+    btnPin = document.createElement('button');
+    btnPin.classList.add('btnPin');
+    btnPin.textContent = 'Скрыть пин со страницы';
+    btnPin.style.cssText = btnStyle;
+
+    btnPin.addEventListener('click', () => {
+        for (let i = 0; i < newImages.length; i++) {
+            newImages = newImages.filter((item) => {
+                if(item.name !== obj.name){
+                    imageOut.addEventListener('click', (event) => {
+                        if (event.target.className != 'btnPin') return;
+                        let pane = event.target.closest('.wrapper');
+                        pane.style.opacity = '0.9';
+                        let hoverPin = document.createElement('div');
+                        hoverPin.classList.add('hoverPin');
+                        hoverPin.innerHTML = 'Вы больше не увидите этот пин'
+                        pane.append(hoverPin)
+                    });
+                    return item;
+                }
+            })
+        }
+        localStorage.setItem('gallery', JSON.stringify(newImages));
+    });
+    
+    //Подключение модального окна "пожаловаться"
+    btnComplaine = document.createElement('button');
+    btnComplaine.classList.add('btnComplaine');
+    btnComplaine.textContent = 'Пожаловаться';
+    btnComplaine.style.cssText = btnStyle;
+
+    const popoutComplain = document.querySelector(".popout_complain");
+    const popoutContent = document.querySelector(".complain_content");
+    const cancelBtn = document.querySelector(".cancelBtn");
+    const nextBtn = document.querySelector(".nextBtn");
+    const nextModal = document.querySelector(".next_modal");
+    const nextModalClose = document.querySelector(".next_modal__close");
+
+
+    btnComplaine.addEventListener('click', function modalComplain(){
+        popoutComplain.style.display = "block";
+    })
+
+    cancelBtn.addEventListener('click', function closeComplain(){
+        popoutComplain.style.display = "none";
+    })
+
+    document.addEventListener('keydown', (e) => {
+       if(e.keyCode === 27){
+              popoutComplain.style.display = "none";
+              nextModal.style.display = "none";
+          }
+        });
+
+    //    второе модальное
+
+    nextBtn.addEventListener('click', function openSecondModal(){
+        nextModal.style.display = "block";
+        popoutComplain.style.display = "none";
+
+    })
+    
+    nextModalClose.addEventListener('click', function closeSecondModal(){
+        nextModal.style.display = "none";
+
+    })
+
     sizer.append(wrapper);
     wrapper.append(imgContainer, about);
+    imgContainer.append(hoverMenu,img);
     about.append(avatar, info);
-    imgContainer.append(img, hoverMenu);
-    hoverMenu.append(btnAdd, btnPin, btnComplaine)
+    hoverMenu.append(btnAdd, btnPin, btnComplaine);
+   
 }
-
-$(document).ready(function () {
-    let container = $(".imageOut");
-    container.imagesLoaded(function () {
-        container.masonry({
-            itemSelector: ".wrapper",
-            columnWidth: ".sizer",
-        });
-    });
-});
-
-
-
-//подключение masonry
-$(document).ready(function () {
-    let container = $(".imageOut");
-    container.imagesLoaded(function () {
-        container.masonry({
-            itemSelector: ".wrapper",
-            columnWidth: ".sizer",
-        });
-    });
-});
-
 
 /*список выбора досок*/
 
-const tabs = document.querySelector('.tabs');
 const content = document.querySelectorAll('.content');
-
 const changeClass = (element) => {
 
     for (let i = 0; i < tabs.children.length; i++) {
@@ -248,43 +331,97 @@ tabs.addEventListener('click', (event) => {
         if (content[i].dataset.content === currTab) {
             content[i].classList.add('active');
         }
+        helper.innerHTML = '';
     }
 });
-<<<<<<< search
 
+function closeModal() {
+    modalElement.classList.add('hide');
+    modalElement.classList.remove('show');
+    document.body.style.overflow = '';
 
-/* поиск */
-//console.log(newImages);
-//console.log(sizer);
+}
 
+//Добавление фото на доску 1
+btnOne.addEventListener('click', () => {
+    let imgDeskOne = {};
+    imgDeskOne.src = currentImg.src;
+    arrDeskOne.push(imgDeskOne);
+    createDeskOne(imgDeskOne)
+    localStorage.setItem('deskOne', JSON.stringify(arrDeskOne));
+    closeModal()
+})
+//Добавление фото на доску 2
+btnTwo.addEventListener('click', () => {
+    let imgDeskTwo = {};
+    imgDeskTwo.src = currentImg.src;
+    arrDeskTwo.push(imgDeskTwo);
+    createDeskTwo(imgDeskTwo)
+    localStorage.setItem('deskTwo', JSON.stringify(arrDeskTwo));
+    closeModal()
+})
+//Добавление фото на доску 3
+btnThree.addEventListener('click', () => {
+    let imgDeskThree = {};
+    imgDeskThree.src = currentImg.src;
+    arrDeskThree.push(imgDeskThree);
+    createDeskThree(imgDeskThree)
+    localStorage.setItem('deskThree', JSON.stringify(arrDeskThree));
+    closeModal()
+})
+
+//search
+let searchImage;
+let searchWrapper;
+const helper = document.querySelector('.helper')//контейнер в который складываются фото выбранные по хештегу
 const searchId = document.getElementById('searchId');
-//console.log(searchId);
 
 searchId.addEventListener('keyup', (e) => {
-
-    const searchString = e.target.value.toLowerCase();
-    console.log(searchString);
-
-    const filteredImgBlocks = newImages.filter((imgBlock) => {
-        return (imgBlock.name.toLowerCase().includes(searchString));
-        //console.log(imgBlock.name.toLowerCase().includes(searchString));
-    });
-
-    displayCharacters(filteredImgBlocks);
-    //console.log(filteredImgBlocks);
+const valueSearch = e.target.value.toLowerCase();
+if (imageOut.innerHTML && valueSearch) { //проверка на присутствие фото на главной странице и на ввод в поиске
+    newImages = JSON.parse(localStorage.getItem('gallery'));//берем данные из local storage
+    helper.innerHTML = ''; // чтобы картиночки стирались при поиске новых
+            newImages.filter((item) => {
+                if(valueSearch === (item.hashtag).toLowerCase().replace('#','')) {//сравниваем то что введено в поиск и хештегом каждого объекта
+                //и если есть совпадения то рисуем это на UI
+                helper.style.cssText = `
+                display: grid;
+                grid-template-columns:repeat(4, 1fr);
+                column-gap: 30px;
+                `
+                imageOut.classList.remove('active');//скрываем главную страницу
+                searchWrapper = document.createElement('div');//создаем новый контейнер
+                searchImage = document.createElement('img');//создаем картинку с такими же данными что из совпадения по поиску
+                searchImage.src = item.name;
+                searchImage.style.cssText = `
+                object-fit: cover;
+                width:100%;
+                height: auto;
+                border-radius:20px;
+                margin-right:30px;
+                `
+                searchWrapper.append(searchImage);  
+                helper.append(searchWrapper);
+                searchId.value = ''
+                } 
+            });  
+} 
+});
+//для того чтобы при нажатии на главную страницу исчезало то что нашлось по хештегу
+const btnMain = document.getElementById(10);
+btnMain.addEventListener('click', () => {
+    imageOut.classList.add('active');
+    helper.innerHTML = ''
 });
 
 
-const displayCharacters = (wrap) => {
-    const htmlString = wrap
-        .map((imgBlock) => {
-            return imgBlock.name;
-        })
-        .join('');
-    //console.log(wrap);
-    sizer.innerHTML = htmlString;
-};
-
-
-=======
->>>>>>> develop
+//подключение masonry
+$(document).ready(function() {
+    let container = $(".imageOut");
+    container.imagesLoaded(function() {
+        container.masonry({
+            itemSelector: ".wrapper",
+            columnWidth: ".sizer",
+        });
+    });
+});
